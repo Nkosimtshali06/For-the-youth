2296a674ede87b86b32e684672dc3abfb231a759
Nkosimtshali06 <138253867+Nkosimtshali06@users.noreply.github.com>
2024-10-15 07:36:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ function closeFullScreenAd() {
 function goToPage() {
     window.location.href = 'https://www.lnksforyou.com/link/42662';
 }
-function showFullscreenAd() {
+function FullscreenAd() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = "https://vdbaa.com/pup.php?section=General&pt=2&pub=719287&ga=g";
